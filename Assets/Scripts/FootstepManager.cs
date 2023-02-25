05dbc062876a84bbebaefafbd3412c34664fac5a
jakobwismar <alerdyy@gmail.com>
2023-02-24 19:08:00 -0500

    Added footsteps

---
+++ Assets/Scripts/FootstepManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepManager : MonoBehaviour
+{
+    [SerializeField] float sprintStepSpeed;
+    
+    [SerializeField] float stepSpeed;
+
+    [SerializeField] float interval;
+
+    [SerializeField] Rigidbody playerRb;
+
+    [SerializeField] PlayerController playerController;
+
+    [SerializeField] float LowPitchRange = .95f;
+	[SerializeField] float HighPitchRange = 1.05f;
+
+    [SerializeField] AudioClip[] footsteps;
+
+    float globalTimer;
+    void Update()
+    {
+        globalTimer += Time.deltaTime*(playerController.sprint ? sprintStepSpeed : stepSpeed);
+    }
+
+    void LateUpdate()
+    {
+        Vector3 playerVelocity = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
+
+        if (playerVelocity.magnitude > 1 && globalTimer > interval)
+        {
+            RandomSoundEffect(footsteps);
+            globalTimer = 0;
+        }
+    }
+
+    public void RandomSoundEffect(params AudioClip[] clips)
+	{
+		int randomIndex = Random.Range(0, clips.Length);
+		float randomPitch = Random.Range(LowPitchRange, HighPitchRange);
+
+        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.pitch = randomPitch;
+		audioSource.clip = clips[randomIndex];
+		audioSource.Play();
+        Destroy(audioSource, audioSource.clip.length);
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] AudioClip[] playerDeath;
+    [SerializeField] AudioClip[] destroy;
+    [SerializeField] AudioClip[] charge;
+    [SerializeField] AudioClip[] shoot;
+
+    [SerializeField] AudioClip[] jump;
+
+    // Music player components
+	[SerializeField] AudioSource MusicSource;
+
+	// Random pitch adjustment range.
+	[SerializeField] float LowPitchRange = .95f;
+	[SerializeField] float HighPitchRange = 1.05f;
+
+	// Singleton instance.
+	public static SoundManager Instance = null;
+	
+	// Initialize the singleton instance.
+	private void Awake()
+	{
+		// If there is not already an instance of SoundManager, set it to this.
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		//If an instance already exists, destroy whatever this object is to enforce the singleton.
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+
+		//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+		DontDestroyOnLoad (gameObject);
+	}
+
+	// Play a single clip through the sound effects source.
+	public void Play(AudioClip clip)
+	{
+        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+		audioSource.clip = clip;
+		audioSource.Play();
+        Destroy(audioSource, audioSource.clip.length);
+	}
+
+	// Play a single clip through the music source.
+	public void PlayMusic(AudioClip clip)
+	{
+		MusicSource.clip = clip;
+		MusicSource.Play();
+	}
+
+	// Play a random clip from an array, and randomize the pitch slightly.
+	public void RandomSoundEffect(params AudioClip[] clips)
+	{
+		int randomIndex = Random.Range(0, clips.Length);
+		float randomPitch = Random.Range(LowPitchRange, HighPitchRange);
+
+        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.pitch = randomPitch;
+		audioSource.clip = clips[randomIndex];
+		audioSource.Play();
+        Destroy(audioSource, audioSource.clip.length);
+    }
+
+    public void PlayPlayerDeath()
+    {
+        RandomSoundEffect(playerDeath);
+    }
+
+    public void PlayDestroy()
+    {
+        RandomSoundEffect(destroy);
+    }
+
+    public void PlayCharge()
+    {
+        RandomSoundEffect(charge);
+    }
+
+    public void PlayShoot()
+    {
+        RandomSoundEffect(shoot);
+    }
+
+    public void PlayJump()
+    {
+        RandomSoundEffect(jump);
+    }
+	
+}
